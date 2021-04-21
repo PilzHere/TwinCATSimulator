d4d8ad436cc852e9b3fed5b4e1bba82b0793ac41
Christian Pilz <svampodling@gmail.com>
2021-04-21 09:28:19 +0200

    First commit.

---
+++ TwinCAT Project1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwinCAT.Ads;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test!");
+            TcAdsClient ads = new TcAdsClient();
+            ads.Connect(541);
+
+            if (ads.IsConnected)
+            {
+                Console.WriteLine("I have connection!");
+            } else
+            {
+                Console.WriteLine("I cant connect!");
+            }
+        }
+    }
+}
